8a44bc7d74a950d5716c827b33999e9894e920ad
luiramgu <luis.ramirez.gutierrez@ucreativa.com>
2021-05-20 20:53:36 -0600

    Tarea De clase #2

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,10 +1,12 @@
 package com.ucreativa;
 
 
+import com.ucreativa.familia.fhernando_ramirez;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        fhernando_ramirez fher = new fhernando_ramirez();
     }
 }
--- src/com/ucreativa/familia/fhernando_ramirez.java
+++ src/com/ucreativa/familia/fhernando_ramirez.java
@@ -1,4 +1,8 @@
 package com.ucreativa.familia;
 
 public class fhernando_ramirez {
+
+    public fhernando_ramirez (){
+        System.out.println("Es Fhernando Ramirez");
+    }
 }
