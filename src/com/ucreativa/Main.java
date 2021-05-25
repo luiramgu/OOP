62011287514ca2680fb61949df33f087f910e6d4
luiramgu <luis.ramirez.gutierrez@ucreativa.com>
2021-05-24 19:44:39 -0600

    Trabajo en clase #3 clases de papa

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,12 +1,13 @@
 package com.ucreativa;
 
 
-import com.ucreativa.familia.Fhernando_ramirez;
+import com.ucreativa.familia.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Fhernando_ramirez fher = new Fhernando_ramirez();
+        Rodolfo papa = new Rodolfo("Yo soy el papa de Fhernando");
     }
 }
---
+++ src/com/ucreativa/familia/Rodolfo.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class Rodolfo {
+
+    public Rodolfo(String saludo){
+        System.out.println(saludo);
+    }
+}
